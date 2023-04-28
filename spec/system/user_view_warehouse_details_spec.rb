2a7c88e3aec855fcdf8a55083929d234f54e5d17
rafaeldejesusl <rafaeldejesuspp@gmail.com>
2023-04-28 12:37:53 -0300

    Adição do teste de exibição de detalhes do galpão

---
+++ spec/system/user_view_warehouse_details_spec.rb
@@ -0,0 +1,26 @@
+require 'rails_helper'
+
+describe 'Usuário vê detalhes de um galpão' do
+	it 'e vê informações adicionais' do
+		# Arrange
+		w = Warehouse.create!(name: 'Aeroporto SP', code: 'GRU', city: 'Guarulhos',
+			area: 100_000, address: 'Avenida do Aeroporto, 1000', cep: '15000-000',
+			description: 'Galpão destinado para cargas internacionais'
+		)
+
+		# Act
+		visit('/')
+		click_on('Aeroporto SP')		
+
+		# Assert
+		expect(page).to have_content('Galpão GRU')
+		expect(page).to have_content('Nome: Aeroporto SP')
+		expect(page).to have_content('Cidade: Guarulhos')
+		expect(page).to have_content('Área: 100000 m²')
+		expect(page).to have_content('Endereço: Avenida do Aeroporto, 1000 CEP: 15000-000')
+		expect(page).to have_content('Galpão destinado para cargas internacionais')
+
+	end
+
+
+end
