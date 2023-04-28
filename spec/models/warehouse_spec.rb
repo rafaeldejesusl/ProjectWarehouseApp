44939169fe5391322a7d76d51f79c3bd5c425b7b
rafaeldejesusl <rafaeldejesuspp@gmail.com>
2023-04-28 17:58:49 -0300

    Adição do teste de obrigatoriedade do nome

--- spec/models/warehouse_spec.rb
+++ spec/models/warehouse_spec.rb
@@ -1,5 +1,21 @@
 require 'rails_helper'
 
 RSpec.describe Warehouse, type: :model do
-  
+  describe '#valid?' do
+		it 'falso quando o nome é vazio' do
+			# Arrange
+			warehouse = Warehouse.new(name: '', code: 'RIO', address: 'Endereço',
+				cep: '25000-000', city: 'Rio', area: 1000,
+				description: 'Alguma descrição')
+
+			# Act
+			result = warehouse.valid?
+
+			# Assert
+			expect(result).to eq false
+		end
+			# Assert
+			expect(result).to eq false
+		end
+	end
 end
