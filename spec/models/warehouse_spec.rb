6fa2334600d82476e6d90c6b6c9a9af2a72ab5f9
rafaeldejesusl <rafaeldejesuspp@gmail.com>
2023-04-28 18:09:21 -0300

    Adição do teste de código único do galpão

--- spec/models/warehouse_spec.rb
+++ spec/models/warehouse_spec.rb
@@ -92,8 +92,19 @@ RSpec.describe Warehouse, type: :model do
 			# Assert
 			expect(result).to eq false
 		end
-			# Assert
-			expect(result).to eq false
-		end
+
+    it 'falso quando o código for repetido' do
+      # Arrange
+      first_warehouse = Warehouse.create!(name: 'Rio', code: 'RIO', address: 'Endereço',
+        cep: '25000-000', city: 'Rio', area: 1000, description: 'Alguma descrição')
+      second_warehouse = Warehouse.new(name: 'Niteroi', code: 'RIO', address: 'Avenida',
+        cep: '35000-000', city: 'Niteroi', area: 1500, description: 'Outra descrição')
+    
+      # Act
+      result = second_warehouse.valid?
+    
+      # Assert
+      expect(result).to eq false
+    end
 	end
 end
