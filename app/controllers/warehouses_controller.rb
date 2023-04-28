2b0cb9a7cab96c6aff5cb6b20dfa20a9390da170
rafaeldejesusl <rafaeldejesuspp@gmail.com>
2023-04-28 17:30:06 -0300

    Adição dos controllers de criação do galpão

--- app/controllers/warehouses_controller.rb
+++ app/controllers/warehouses_controller.rb
@@ -2,4 +2,20 @@ class WarehousesController < ApplicationController
 	def show
 		@warehouse = Warehouse.find(params[:id])
 	end
+
+	def new
+		@warehouse = Warehouse.new()
+	end
+	
+	def create
+		warehouse_params = params.require(:warehouse).permit(:name, :code, :city,
+			:description, :address, :cep, :area)
+		@warehouse = Warehouse.new(warehouse_params)
+		if @warehouse.save()
+			redirect_to root_path, notice: 'Galpão cadastrado com sucesso.'
+		else
+			flash.now[:notice] = 'Galpão não cadastrado.'
+			render 'new'
+		end	
+	end
 end
