6739db3fc4dc6439d60b555b1dc4790ddbbe9fee
rafaeldejesusl <rafaeldejesuspp@gmail.com>
2023-04-28 17:31:36 -0300

    Adição dos testes de criação com sucesso e incompleto

--- spec/system/user_register_warehouse_spec.rb
+++ spec/system/user_register_warehouse_spec.rb
@@ -18,4 +18,41 @@ describe 'Usuário cadastra um galpão' do
 		expect(page).to have_field('Área')
 	end
 
+  it 'com sucesso' do
+    # Arrange
+  
+    # Act
+    visit root_path
+    click_on 'Cadastrar Galpão'
+    fill_in 'Nome', with: 'Rio de Janeiro'
+    fill_in 'Descrição', with: 'Galpão da zona portuária do Rio'
+    fill_in 'Código', with: 'RIO'
+    fill_in 'Endereço', with: 'Avenida do Museu do Amanhã, 1000'
+    fill_in 'Cidade', with: 'Rio de Janeiro'
+    fill_in 'CEP', with: '20100-000'
+    fill_in 'Área', with: '32000'
+    click_on 'Enviar'
+  
+    # Assert
+    expect(current_path).to eq root_path
+    expect(page).to have_content 'Galpão cadastrado com sucesso.'
+    expect(page).to have_content 'Rio de Janeiro'
+    expect(page).to have_content 'RIO'
+    expect(page).to have_content '32000 m²'
+  end
+
+  it 'com dados imcompletos' do
+    # Arrange
+  
+    # Act
+    visit root_path
+    click_on 'Cadastrar Galpão'
+    fill_in 'Nome', with: ''
+    fill_in 'Descrição', with: ''
+    fill_in 'Código', with: ''
+    click_on 'Enviar'
+  
+    # Assert
+    expect(page).to have_content 'Galpão não cadastrado.'
+  end
 end
