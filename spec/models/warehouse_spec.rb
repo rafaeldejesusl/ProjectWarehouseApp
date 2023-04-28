5c03ef1ff3cba0c1665656eb6150f9e5a46c1f36
rafaeldejesusl <rafaeldejesuspp@gmail.com>
2023-04-28 18:05:04 -0300

    Adição do teste de obrigatoriedade da descrição

--- spec/models/warehouse_spec.rb
+++ spec/models/warehouse_spec.rb
@@ -79,6 +79,19 @@ RSpec.describe Warehouse, type: :model do
 			# Assert
 			expect(result).to eq false
 		end
+
+    it 'falso quando a descrição é vazia' do
+			# Arrange
+			warehouse = Warehouse.new(name: 'Rio de Janeiro', code: 'RIO', address: 'Endereço',
+				cep: '25000-000', city: 'Rio', area: 1000,
+				description: '')
+
+			# Act
+			result = warehouse.valid?
+
+			# Assert
+			expect(result).to eq false
+		end
 			# Assert
 			expect(result).to eq false
 		end
