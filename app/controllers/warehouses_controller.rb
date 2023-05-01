1e9c7c7ddb9ef1cefed65813d834aa2818583212
rafaeldejesusl <rafaeldejesuspp@gmail.com>
2023-05-01 17:40:18 -0300

    Adição da edição de galpões

--- app/controllers/warehouses_controller.rb
+++ app/controllers/warehouses_controller.rb
@@ -18,4 +18,24 @@ class WarehousesController < ApplicationController
 			render 'new'
 		end	
 	end
+
+	def edit
+		id = params[:id]
+		@warehouse = Warehouse.find(id)
+	end
+
+	def update
+		id = params[:id]
+		@warehouse = Warehouse.find(id)
+	
+		warehouse_params = params.require(:warehouse).permit(:name, :code, :city,
+			:description, :address, :cep, :area
+		)
+		if @warehouse.update(warehouse_params)
+			redirect_to warehouse_path(@warehouse.id), notice: 'Galpão atualizado com sucesso'
+		else
+			flash.now[:notice] = 'Não foi possível atualizar o galpão'
+			render 'edit'
+		end
+	end
 end
