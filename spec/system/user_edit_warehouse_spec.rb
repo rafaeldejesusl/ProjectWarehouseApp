edb82a157b1339d4d05097009de9a1742c288cdf
rafaeldejesusl <rafaeldejesuspp@gmail.com>
2023-05-01 17:41:14 -0300

    Complementação dos testes de edição de galpões

--- spec/system/user_edit_warehouse_spec.rb
+++ spec/system/user_edit_warehouse_spec.rb
@@ -24,4 +24,48 @@ describe 'Usuário edita um galpão' do
 		expect(page).to have_field('Área', with: '100000')
 	end
 
+  it 'com sucesso' do
+    # Arrange
+    w = Warehouse.create!(name: 'Aeroporto SP', code: 'GRU', city: 'Guarulhos',
+      area: 100_000, address: 'Avenida do Aeroporto, 1000', cep: '15000-000',
+      description: 'Galpão destinado para cargas internacionais'
+    )
+  
+    # Act
+    visit root_path
+    click_on 'Aeroporto SP'
+    click_on 'Editar'
+    fill_in 'Nome', with: 'Galpão Internacional'
+    fill_in 'Área', with: '200000'
+    fill_in 'Endereço', with: 'Avenida dos Galpões, 500'
+    fill_in 'CEP', with: '16000-000'
+    click_on 'Enviar'
+  
+    # Assert
+    expect(page).to have_content 'Galpão atualizado com sucesso'
+    expect(page).to have_content 'Nome: Galpão Internacional'
+    expect(page).to have_content 'Endereço: Avenida dos Galpões, 500'
+    expect(page).to have_content 'Área: 200000 m²'
+    expect(page).to have_content 'CEP: 16000-000'
+  end
+
+  it 'e mantém os campos obrigatórios' do
+    # Arrange
+    w = Warehouse.create!(name: 'Aeroporto SP', code: 'GRU', city: 'Guarulhos',
+      area: 100_000, address: 'Avenida do Aeroporto, 1000', cep: '15000-000',
+      description: 'Galpão destinado para cargas internacionais'
+    )
+  
+    # Act
+    visit root_path
+    click_on 'Aeroporto SP'
+    click_on 'Editar'
+    fill_in 'Nome', with: ''
+    fill_in 'Área', with: ''
+    fill_in 'Endereço', with: ''
+    click_on 'Enviar'
+  
+    # Assert
+    expect(page).to have_content 'Não foi possível atualizar o galpão'
+  end
 end
