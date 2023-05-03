79cd3787f7cb512db10c6320ee5ff83a92c5055e
rafaeldejesusl <rafaeldejesuspp@gmail.com>
2023-05-03 15:49:53 -0300

    Adição de testes de exibição dos detalhes do fornecedor

---
+++ spec/system/supplier/user_view_supplier_details_spec.rb
@@ -0,0 +1,24 @@
+require 'rails_helper'
+
+describe 'Usuário vê detalhes de um fornecedor' do
+	it 'e vê informações adicionais' do
+		# Arrange
+		Supplier.create!(brand_name: 'Samsung', corporate_name: 'Samsung Eletronicos LTDA',
+      registration_number: '07317108000151', full_address: 'Av Nacoes Unidas, 1000',
+      city: 'São Paulo', state: 'SP', email: 'sac@samsung.com.br')
+    
+		# Act
+		visit root_path
+		click_on('Fornecedores')
+    click_on('Samsung')	
+
+		# Assert
+		expect(page).to have_content('Samsung')
+		expect(page).to have_content('Razão Social: Samsung Eletronicos LTDA')
+		expect(page).to have_content('Cidade: São Paulo')
+		expect(page).to have_content('Estado: SP')
+		expect(page).to have_content('Endereço: Av Nacoes Unidas, 1000')
+		expect(page).to have_content('CNPJ: 07317108000151')
+    expect(page).to have_content('E-mail: sac@samsung.com.br')
+	end
+end
