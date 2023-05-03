d879f4dab091521264a2a6cb25f63b35a7e20012
rafaeldejesusl <rafaeldejesuspp@gmail.com>
2023-05-03 15:14:59 -0300

    Adição de fornecedores nos seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -9,3 +9,9 @@ Warehouse.create!(name: 'Rio', code: 'SDU', city: 'Rio de Janeiro', area: 60_000
   address: 'Av Oceânica, 500', cep: '20000-000', description: 'Galpão no Rio')
 Warehouse.create!(name: 'Maceió', code: 'MCZ', city: 'Maceió', area: 50_000,
   address: 'Av da Praia, 800', cep: '56000-000', description: 'Galpão em Maceió')
+Supplier.create!(brand_name: 'Samsung', corporate_name: 'Samsung Eletronicos LTDA',
+  registration_number: '07317108000151', full_address: 'Av Nacoes Unidas, 1000',
+  city: 'São Paulo', state: 'SP', email: 'sac@samsung.com.br')
+Supplier.create!(brand_name: 'LG', corporate_name: 'LG do Brasil LTDA',
+  registration_number: '34356508000149', full_address: 'Av Ibirapuera, 300',
+  city: 'Guarulhos', state: 'SP', email: 'contato@lg.com.br')
