02de5b98bff08133ba3232790bdf3c98687be246
rafaeldejesusl <rafaeldejesuspp@gmail.com>
2023-05-03 15:50:32 -0300

    Adição da rota de exibição dos detalhes do fornecedor

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,5 @@
 Rails.application.routes.draw do
   root to: 'home#index'
   resources :warehouses, only: [:show, :new, :create, :edit, :update, :destroy]
-  resources :suppliers, only: [:index]
+  resources :suppliers, only: [:index, :show]
 end
