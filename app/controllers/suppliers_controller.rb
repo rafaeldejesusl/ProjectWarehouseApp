9b9d74babca7019704664fe7d5dd9fa628aa1625
rafaeldejesusl <rafaeldejesuspp@gmail.com>
2023-05-03 19:46:56 -0300

    Adição da página de criação do fornecedor

--- app/controllers/suppliers_controller.rb
+++ app/controllers/suppliers_controller.rb
@@ -6,4 +6,20 @@ class SuppliersController < ApplicationController
   def show
 		@supplier = Supplier.find(params[:id])
 	end
+
+	def new
+		@supplier = Supplier.new()
+	end
+
+	def create
+		supplier_params = params.require(:supplier).permit(:brand_name, :corporate_name, :city,
+			:state, :address, :registration_number, :email)
+		@supplier = Supplier.new(supplier_params)
+		if @supplier.save()
+			redirect_to suppliers_path, notice: 'Fornecedor cadastrado com sucesso'
+		else
+			flash.now[:notice] = 'Fornecedor não cadastrado'
+			render 'new'
+		end	
+	end
 end
