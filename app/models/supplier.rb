27c74813073a03c93bde262bf8b0fdb18af4e8a8
rafaeldejesusl <rafaeldejesuspp@gmail.com>
2023-05-08 20:47:24 -0300

    Adição da exibição de produtos na página do fornecedor

--- app/models/supplier.rb
+++ app/models/supplier.rb
@@ -2,4 +2,5 @@ class Supplier < ApplicationRecord
   validates :brand_name, :corporate_name, :registration_number, :full_address, :city, :state, :email, presence: true
   validates :registration_number, uniqueness: true
   validates :registration_number, length: { is: 13 }
+  has_many :product_models
 end
