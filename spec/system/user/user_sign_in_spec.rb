99632d17f8b7565b04b46530e8cccf28a0439459
rafaeldejesusl <rafaeldejesuspp@gmail.com>
2023-05-09 15:12:02 -0300

    Configuração da autenticação do Devise

---
+++ spec/system/user/user_sign_in_spec.rb
@@ -0,0 +1,26 @@
+require 'rails_helper'
+
+describe 'Usuário se autentica' do
+	it 'com sucesso' do
+		# Arrange
+		User.create!(email: 'joao@email.com', password: 'password')
+
+		# Act
+		visit root_path
+		click_on 'Entrar'
+		fill_in 'E-mail', with: 'joao@email.com'
+		fill_in 'Senha', with: 'password'
+		within('form') do
+			click_on 'Entrar'
+		end
+
+		# Assert
+		expect(page).not_to have_link 'Entrar'
+		expect(page).to have_button 'Sair'
+		within('nav') do
+			expect(page).to have_content 'joao@email.com'
+		end
+		expect(page).to have_content 'Login efetuado com sucesso.'
+	end
+  
+end
