473a39cdba40aae4e55e325ffbf29f4bbc13cb0a
rafaeldejesusl <rafaeldejesuspp@gmail.com>
2023-05-22 19:44:23 -0300

    Implementação da funcionalidade de busca por vários pedidos

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -22,6 +22,6 @@ class OrdersController < ApplicationController
 
 	def search
 		@code = params['query']
-		@order = Order.find_by(code: params['query'])
+		@orders = Order.where("code LIKE ?", "%#{@code}%")
 	end
 end
