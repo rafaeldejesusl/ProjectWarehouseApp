010fc7a2e20f6386723ca0a140c411a325d332e8
rafaeldejesusl <rafaeldejesuspp@gmail.com>
2023-05-25 17:34:31 -0300

    Implementação da edição de pedidos

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -1,5 +1,6 @@
 class OrdersController < ApplicationController
 	before_action :authenticate_user!
+	before_action :set_order_and_check_user, only: [:show, :edit, :update]
 
 	def new
 		@order = Order.new
@@ -18,6 +19,9 @@ class OrdersController < ApplicationController
 
 	def show
 		@order = Order.find(params[:id])
+		if @order.user != current_user
+			redirect_to root_path, alert: 'Você não possui acesso a este pedido.'
+		end
 	end
 
 	def search
@@ -27,5 +31,35 @@ class OrdersController < ApplicationController
 
 	def index
 		@orders = current_user.orders
+	end
+
+	def edit
+		@order = Order.find(params[:id])
+		if @order.user != current_user
+			redirect_to root_path, alert: 'Você não possui acesso a este pedido.'
+		end
+		@warehouses = Warehouse.all
+		@suppliers = Supplier.all
+	end
+	
+	def update
+		order_params = params.require(:order).permit(:warehouse_id, :supplier_id,
+				:estimated_delivery_date)
+		@order = Order.find(params[:id])
+		if @order.user != current_user
+			return redirect_to root_path, alert: 'Você não possui acesso a este pedido.'
+		end
+		@order.update(order_params)
+		
+		redirect_to @order, notice: 'Pedido atualizado com sucesso.'
+	end
+
+	private
+
+	def set_order_and_check_user
+		@order = Order.find(params[:id])
+		if @order.user != current_user
+			return redirect_to root_path, alert: 'Você não possui acesso a este pedido.'
+		end
 	end
 end
